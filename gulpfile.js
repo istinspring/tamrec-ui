0ba46bf49703196041371fb00590ad2d209d1d55
istinspring <istinspring@gmail.com>
2015-02-11 17:36:12 +0700

    generate demo for gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,30 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var expect = require('gulp-expect-file');
 var sourcemaps = require('gulp-sourcemaps');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
 var webserver = require('gulp-webserver');
 
-var lessAssets = 'tamrec-ui/less/main.less';
 var assetsDest = 'tamrec-ui/build';
+var ghPages = 'gh-pages';
+var lessAssets = 'tamrec-ui/less/main.less';
+var jsAssets = [
+    'tamrec-ui/js/*.js',
+    'tamrec-ui/js/**/*.js'
+];
+var vendorsCSS = [
+    'tamrec-ui/vendors/angular-material/angular-material.css'
+];
+var vendorsJS = [
+    'tamrec-ui/vendors/angular/angular.js',
+    'tamrec-ui/vendors/angular-aria/angular-aria.js',
+    'tamrec-ui/vendors/angular-animate/angular-animate.js',
+    'tamrec-ui/vendors/angular-material/angular-material.js'
+];
 
 gulp.task('watch', function() {
     gulp.watch('./tamrec-ui/less/*.less', ['compile-less']);
@@ -23,6 +41,40 @@ gulp.task('compile-less', function() {
             .pipe(gulp.dest(assetsDest));
 });
 
+gulp.task('process-js', function() {
+    return gulp
+        .src(jsAssets)
+        .pipe(ngAnnotate())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(assetsDest));
+});
+
+gulp.task('vendors-css', function() {
+    return gulp
+            .src(vendorsCSS)
+            .pipe(expect(vendorsCSS))
+            .pipe(sourcemaps.init())
+            .pipe(minifyCSS({keepBreaks:true}))
+            .pipe(concat('vendors.min.css'))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(assetsDest));
+});
+
+gulp.task('vendors-js', function() {
+    return gulp
+        .src(vendorsJS)
+        .pipe(expect(vendorsJS))
+        .pipe(ngAnnotate())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat('vendors.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(assetsDest));
+});
+
 gulp.task('serve', ['watch'], function() {
     gulp.src('./tamrec-ui')
         .pipe(webserver({
@@ -31,3 +83,24 @@ gulp.task('serve', ['watch'], function() {
             open: true
         }));
 });
+
+gulp.task('see', function () {
+    gulp.src('./gh-pages')
+    .pipe(webserver({
+        livereload: false,
+        directoryListing: false,
+        open: true
+    }));
+});
+
+gulp.task('html',
+    [
+        'compile-less', 'process-js',
+        'vendors-css', 'vendors-js'
+    ],
+    function () {
+        gulp.src('./tamrec-ui/build/*.min.js').pipe(gulp.dest('./gh-pages/build'));
+        gulp.src('./tamrec-ui/build/*.min.css').pipe(gulp.dest('./gh-pages/build'));
+        gulp.src('./tamrec-ui/index.html').pipe(gulp.dest(ghPages));
+    }
+);
