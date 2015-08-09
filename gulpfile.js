a8cd7d7d858dc229f14b1f63d7787d52e4bdcca2
istinspring <istinspring@gmail.com>
2015-08-09 21:32:20 +0700

    update libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require('gulp');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
@@ -12,106 +14,105 @@ var webserver = require('gulp-webserver');
 var assetsDest = 'tamrec-ui/build';
 var ghPages = 'gh-pages';
 var lessAssets = [
-    'tamrec-ui/less/main.less',
-    'tamrec-ui/less/loading-bar.less'
+  'tamrec-ui/less/main.less',
+  'tamrec-ui/less/loading-bar.less',
 ];
 var jsAssets = [
-    'tamrec-ui/js/*.js',
-    'tamrec-ui/js/**/*.js'
+  'tamrec-ui/js/*.js',
+  'tamrec-ui/js/**/*.js',
 ];
 var vendorsCSS = [
-    'tamrec-ui/vendors/angular-material/angular-material.css'
+  'tamrec-ui/vendors/angular-material/angular-material.css',
 ];
 var vendorsJS = [
-    'tamrec-ui/vendors/angular/angular.js',
-    'tamrec-ui/vendors/angular-aria/angular-aria.js',
-    'tamrec-ui/vendors/angular-animate/angular-animate.js',
-    'tamrec-ui/vendors/angular-ui-router/release/angular-ui-router.js',
-    'tamrec-ui/vendors/angular-material/angular-material.js',
-    'tamrec-ui/vendors/angular-loading-bar/build/loading-bar.js'
+  'tamrec-ui/vendors/angular/angular.js',
+  'tamrec-ui/vendors/angular-aria/angular-aria.js',
+  'tamrec-ui/vendors/angular-animate/angular-animate.js',
+  'tamrec-ui/vendors/angular-ui-router/release/angular-ui-router.js',
+  'tamrec-ui/vendors/angular-material/angular-material.js',
+  'tamrec-ui/vendors/angular-loading-bar/build/loading-bar.js',
 ];
 
 gulp.task('watch', function() {
-    gulp.watch('./tamrec-ui/less/*.less', ['compile-less']);
-    gulp.watch('./tamrec-ui/js/*.js', ['process-js']);
-    gulp.watch('./tamrec-ui/js/**/*.js', ['process-js']);
+  gulp.watch('./tamrec-ui/less/*.less', ['compile-less']);
+  gulp.watch('./tamrec-ui/js/*.js', ['process-js']);
+  gulp.watch('./tamrec-ui/js/**/*.js', ['process-js']);
 });
 
 gulp.task('compile-less', function() {
-    return gulp
-            .src(lessAssets)
-            .pipe(expect(lessAssets))
-            .pipe(sourcemaps.init())
-            .pipe(less({compress: true}))
-            .pipe(concat('app.min.css'))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest(assetsDest));
+  return gulp
+  .src(lessAssets)
+  .pipe(expect(lessAssets))
+  .pipe(sourcemaps.init())
+  .pipe(less({compress: true}))
+  .pipe(concat('app.min.css'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(assetsDest));
 });
 
 gulp.task('process-js', function() {
-    return gulp
-        .src(jsAssets)
-        .pipe(ngAnnotate())
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(concat('app.min.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(assetsDest));
+  return gulp
+  .src(jsAssets)
+  .pipe(ngAnnotate())
+  .pipe(sourcemaps.init())
+  .pipe(uglify())
+  .pipe(concat('app.min.js'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(assetsDest));
 });
 
 gulp.task('vendors-css', function() {
-    return gulp
-            .src(vendorsCSS)
-            .pipe(expect(vendorsCSS))
-            .pipe(sourcemaps.init())
-            .pipe(minifyCSS({keepBreaks:true}))
-            .pipe(concat('vendors.min.css'))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest(assetsDest));
+  return gulp
+  .src(vendorsCSS)
+  .pipe(expect(vendorsCSS))
+  .pipe(sourcemaps.init())
+  .pipe(minifyCSS({keepBreaks:true}))
+  .pipe(concat('vendors.min.css'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(assetsDest));
 });
 
 gulp.task('vendors-js', function() {
-    return gulp
-        .src(vendorsJS)
-        .pipe(expect(vendorsJS))
-        .pipe(ngAnnotate())
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(concat('vendors.min.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(assetsDest));
+  return gulp
+  .src(vendorsJS)
+  .pipe(expect(vendorsJS))
+  .pipe(ngAnnotate())
+  .pipe(sourcemaps.init())
+  .pipe(uglify())
+  .pipe(concat('vendors.min.js'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(assetsDest));
 });
 
 gulp.task('serve', [
-    'watch', 'compile-less', 'process-js',
-    'vendors-css', 'vendors-js'
-    ],
-    function() {
-        gulp.src('./tamrec-ui')
-            .pipe(webserver({
-                livereload: true,
-                directoryListing: false,
-                open: true
-            }));
+  'watch', 'compile-less', 'process-js',
+  'vendors-css', 'vendors-js',
+],
+function() {
+  gulp.src('./tamrec-ui')
+  .pipe(webserver({
+    livereload: true,
+    directoryListing: false,
+    open: true,
+  }));
 });
 
-gulp.task('see', function () {
-    gulp.src('./gh-pages')
-    .pipe(webserver({
-        livereload: false,
-        directoryListing: false,
-        open: true
-    }));
+gulp.task('see', function() {
+  gulp.src('./gh-pages')
+  .pipe(webserver({
+    livereload: false,
+    directoryListing: false,
+    open: true,
+  }));
 });
 
 gulp.task('html',
-    [
-        'compile-less', 'process-js',
-        'vendors-css', 'vendors-js'
-    ],
-    function () {
-        gulp.src('./tamrec-ui/build/*.min.js').pipe(gulp.dest('./gh-pages/build'));
-        gulp.src('./tamrec-ui/build/*.min.css').pipe(gulp.dest('./gh-pages/build'));
-        gulp.src('./tamrec-ui/index.html').pipe(gulp.dest(ghPages));
-    }
-);
+[
+  'compile-less', 'process-js',
+  'vendors-css', 'vendors-js',
+],
+function() {
+  gulp.src('./tamrec-ui/build/*.min.js').pipe(gulp.dest('./gh-pages/build'));
+  gulp.src('./tamrec-ui/build/*.min.css').pipe(gulp.dest('./gh-pages/build'));
+  gulp.src('./tamrec-ui/index.html').pipe(gulp.dest(ghPages));
+});
